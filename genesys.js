ecced6ea0210a0e1753fee2972ae514249d67153
sudanamrinder-debug <sudan.amrinder@gmail.com>
2026-05-14 22:09:40 +0530

    Update genesys.js

--- genesys.js
+++ genesys.js
@@ -1,26 +1,26 @@
 (function (g, e, n, es, ys) {
+
   g['_genesysJs'] = e;
+
   g[e] = g[e] || function () {
     (g[e].q = g[e].q || []).push(arguments)
   };
+
   g[e].t = 1 * new Date();
   g[e].c = es;
+
   ys = document.createElement('script');
   ys.async = 1;
   ys.src = n;
   ys.charset = 'utf-8';
+
   document.head.appendChild(ys);
-})(window, 'Genesys', 'https://apps.aps1.pure.cloud/genesys-bootstrap/genesys.min.js', {
-  environment: 'prod-aps1',
-  deploymentId: 'b81334ac-9fc1-41b3-a1db-de025c277a0c'
-});
 
-window.onload = function () {
-
-  Genesys("command", "Journey.record", {
-    eventName: "test_offer"
-  });
-
-  console.log("Journey event sent");
-
-};
+})(window,
+   'Genesys',
+   'https://apps.aps1.pure.cloud/genesys-bootstrap/genesys.min.js',
+   {
+      environment: 'prod-aps1',
+      deploymentId: 'b81334ac-9fc1-41b3-a1db-de025c277a0c'
+   }
+);
