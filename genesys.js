02f2c16206c466fbc61475426ac3ea341387c4d8
sudanamrinder-debug <sudan.amrinder@gmail.com>
2026-05-14 21:24:24 +0530

    Update genesys.js

--- genesys.js
+++ genesys.js
@@ -1,16 +1,14 @@
-(function (g, e, n, es, ys) {
-  g['_genesysJs'] = e;
-  g[e] = g[e] || function () {
-    (g[e].q = g[e].q || []).push(arguments)
-  };
-  g[e].t = 1 * new Date();
-  g[e].c = es;
-  ys = document.createElement('script');
-  ys.async = 1;
-  ys.src = n;
-  ys.charset = 'utf-8';
-  document.head.appendChild(ys);
-})(window, 'Genesys', 'https://apps.aps1.pure.cloud/genesys-bootstrap/genesys.min.js', {
-  environment: 'prod-aps1',
-  deploymentId: 'b81334ac-9fc1-41b3-a1db-de025c277a0c'
-});
+<script type="text/javascript" charset="utf-8">
+  (function (g, e, n, es, ys) {
+    g['_genesysJs'] = e;
+    g[e] = g[e] || function () {
+      (g[e].q = g[e].q || []).push(arguments)
+    };
+    g[e].t = 1 * new Date();
+    g[e].c = es;
+    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+  })(window, 'Genesys', 'https://apps.aps1.pure.cloud/genesys-bootstrap/genesys.min.js', {
+    environment: 'prod-aps1',
+    deploymentId: 'b81334ac-9fc1-41b3-a1db-de025c277a0c'
+  });
+</script>
